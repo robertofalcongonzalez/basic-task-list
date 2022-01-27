6890e912fe28aa38756dabac7fa129c189944b91
Roberto <robertofalcongonzalez@gmail.com>
2022-01-26 23:02:14 -0500

    Installing feather icons component library

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import {createApp} from 'vue';
 import App from './App.vue';
 import './index.css';
+import VueFeather from 'vue-feather'
 
-createApp(App).mount('#app')
+const vueApp = createApp(App)
+vueApp.component(VueFeather.name as string, VueFeather)
+vueApp.mount('#app')
