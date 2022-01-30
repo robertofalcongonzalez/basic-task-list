76cca994a9d7c0ba363e978041bf6732056d6421
Roberto <robertofalcongonzalez@gmail.com>
2022-01-30 01:44:29 -0500

    setting the environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#04142F',
+        secondary: '#EAF0F5',
+        tertiary: '#8A94A6',
+        'hash-text': '#702EE6',
+
+      },
+      spacing: {
+        '0.5': '2px',
+        '3.5': '14.7px',
+        '14px': '14px'
+      },
+      screens: {
+        'lg': '1230px',
+      }
+    },
   },
   plugins: [],
 }
